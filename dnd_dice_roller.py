ce14cb84f566ab9136c7d60a21b0622f87e37fcc
Eric Halina <kaifinlol@gmail.com>
2020-09-01 00:55:40 -0600

    updated dice roller to handle any number of dice! still need to fix 2d0 case

--- dnd_dice_roller.py
+++ dnd_dice_roller.py
@@ -1,5 +1,5 @@
-# dnd dice roller v0.5, with parsing of Ax + b format
-# TODO: add handling for any order / combination of die rolls (ie: 2d4 + 3d8 + 2d6 + 7 + 9)
+# D&D dice roller v1.0
+# takes in a list of dice
 
 import random
 
@@ -11,94 +11,85 @@ def get_input():
     return input('Enter your dice roll! ')
 
 def parse_dice_rolls(input):
-    '''
-    Parses and rolls dice for Dungeons and Dragons.
-    input: string representing a # of dice to roll and a modifier if needed (ie 3d6 + 5)
-    output: a formatted string representing the result of the roll
-    '''
-    input = input.replace(' ', '')
-    success = True
-    roll_log = []
-    # get the prefix
-    try:
-        prefix = ''
-        i = 0
-        while input[i].lower() != 'd':
-            prefix += input[i]
-            i += 1
-        # if there is no prefix, roll the dice one time
-        if prefix == '':
-            prefix = 1
-        prefix = int(prefix)
 
-    except IndexError:
-        print('invalid prefix!')
-        success = False
-    except TypeError:
-        print('invalid prefix!')
-        success = False
-    else:
-        # get the number of sides for the dice to have
-        try:
-            i += 1 # getting past 'd' here
-            faces = ''
-            # get the number of faces
-            while input[i] not in ['+', '-'] and i < len(input)-1:
-                faces += input[i]
+    # parse the input into a list
+    input = input.replace(' ', '')
+    input_list = []
+    chunk = ''
+    for c in input:
+        if c != '+' and c != '-':
+            chunk += c
+        else:
+            input_list.append(chunk)
+            input_list.append(c)
+            chunk = ''
+
+    if chunk != '':
+        input_list.append(chunk)
+
+    running_total = []
+    roll_log = []
+    # process the input
+    for item in input_list:
+        item = item.lower()
+        # this is a dice
+        if 'd' in item:
+            roll_log.append(item + ':')
+            # find prefix (if there is one)
+            i = 0
+            prefix = ''
+            while item[i] != 'd':
+                prefix += item[i]
                 i += 1
-            if input[i] not in ['+', '-']:
-                faces += input[i]
-            faces = int(faces)
-            dice = str(prefix) + 'd' + str(faces) + ':'
-            roll_log.append(dice)
-            # roll the dice the correct number of times (according to prefix)
-            total = 0
+            try:
+                prefix = int(prefix)
+            except:
+                print('Whoops prefix')
+                return 'Error! Please double-check your formatting and try again!'
+
+            # get number of faces
+            i += 1
+            faces = ''
+            while i < len(item):
+                faces += item[i]
+                i += 1
+            try:
+                faces = int(faces)
+            except:
+                print('Whoops faces')
+                return 'Error! Please double-check your formatting and try again!'
+
+            die_total = 0
+            # roll the dice
             for k in range(prefix):
                 roll = random.randint(1, faces)
-                total += roll
+                die_total += roll
                 roll_log.append('(' + str(roll) + ')')
-
-        except Exception as e:
-            print(e.args[0])
-            success = False
+            running_total.append(die_total)
 
         else:
-            # handle the modifier
-            add_sub = None
-            if i <= len(input)-1 and input[i] in ['+', '-']:
-                if input[i] == '+':
-                    add_sub = 'add'
-                else:
-                    add_sub = 'sub'
+            roll_log.append(item)
+            running_total.append(item)
 
-            # get the modifier
-            modifier = ''
-            i += 1
-            while i <= len(input)-1:
-                modifier += input[i]
-                i += 1
+    # tally up the running total accounting for signs
+    add_sub = True
+    total = 0
+    for term in running_total:
+        if term != '+' and term != '-':
+            if add_sub:
+                total += int(term)
+            else:
+                total -= int(term)
+        else:
+            if term == '+':
+                add_sub = True
+            else:
+                add_sub = False
 
-            # add or subtract the modifier, if there is one
-            if modifier == '':
-                modifier = '0'
-            modifier = int(modifier)
-            if add_sub == 'add':
-                total += modifier
-                roll_log.append('+')
-            elif add_sub == 'sub':
-                total -= modifier
-                roll_log.append('-')
-            if modifier != 0:
-                roll_log.append(str(modifier))
-            roll_log.append('total: ' + str(total))
+    roll_log.append('Total: ' + str(total))
+    print(' '.join(roll_log))
+    return ' '.join(roll_log)
 
-    # finish up, returning the correctly formatted log of the roll
-    if success:
-        print(' '.join(roll_log))
-        return ' '.join(roll_log)
-    else:
-        print('failure! Abort Abort')
-        return 'Error! Please follow the format of AdB + C until I have enough brain cells to make it work for any format'
 
 if __name__ == "__main__":
     main()
